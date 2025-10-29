61179547769c7435cec973e58a8f42c8757b98eb
Yogi Wirmanda <wirmanda@gmail.com>
2025-10-29 14:52:42 +0700

    Update journalService.ts

--- src/services/journalService.ts
+++ src/services/journalService.ts
@@ -4,7 +4,7 @@ import api from './api'
 export const journalService = {
 
   async getAll(userId: number): Promise<ForumResponse> {
-    const { data } = await api.get<ForumResponse>(`/journal/lists?user_id=${userId}&questionnaire_id=3`)
+    const { data } = await api.get<ForumResponse>(`/journal/lists?user_id=${userId}`)
     return data
   },
 }
