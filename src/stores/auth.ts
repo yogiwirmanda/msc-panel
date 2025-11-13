ad3690acbd1f7195fbd556a06a23f6be3eec78a6
Yogi Wirmanda <wirmanda@gmail.com>
2025-11-13 08:55:20 +0700

    update

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,159 +1,161 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-import { authService, type RegisterForm } from '../services/authService'
-import { useAppToast } from '../composables/useAppToast'
-import Cookies from 'js-cookie'
-import { useRouter } from 'vue-router'
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import { authService, type RegisterForm } from "../services/authService";
+import { useAppToast } from "../composables/useAppToast";
+import Cookies from "js-cookie";
+import { useRouter } from "vue-router";
 
-export const useAuthStore = defineStore('auth', () => {
-  const members = ref<RegisterForm[]>([])
-  const loading = ref(false)
-  const error = ref<string | null>(null)
-  const data = ref<any>(null)
-  const { showToast } = useAppToast()
-  const token = ref<string | null>(Cookies.get('token') || null)
-  const isLogin = ref(false)
-  const loginRole = ref('')
-  const userLogin = ref<any>()
-  const listAgreements = ref<any>()
+export const useAuthStore = defineStore("auth", () => {
+  const members = ref<RegisterForm[]>([]);
+  const loading = ref(false);
+  const error = ref<string | null>(null);
+  const data = ref<any>(null);
+  const { showToast } = useAppToast();
+  const token = ref<string | null>(Cookies.get("token") || null);
+  const isLogin = ref(false);
+  const loginRole = ref("");
+  const userLogin = ref<any>();
+  const listAgreements = ref<any>();
   const router = useRouter();
 
   const setToken = (value: string) => {
-    token.value = value
-    Cookies.set('token', value)
-  }
+    token.value = value;
+    Cookies.set("token", value);
+  };
 
   const clearToken = () => {
-    token.value = null
-    Cookies.remove('token')
-    Cookies.remove('user')
-    Cookies.remove('role')
-  }
+    token.value = null;
+    Cookies.remove("token");
+    Cookies.remove("user");
+    Cookies.remove("role");
+  };
 
   const initialize = async () => {
-    loading.value = true
+    loading.value = true;
     try {
-      const response = await authService.initialize()
-      userLogin.value = await response.data.user
-      Cookies.set('user', JSON.stringify(userLogin.value));
+      const response = await authService.initialize();
+      userLogin.value = await response.data.user;
+      Cookies.set("user", JSON.stringify(userLogin.value));
     } catch (err: any) {
-      if (err.status == 401){
+      if (err.status == 401) {
         logoutMember();
-        router.push('/login')
+        router.push("/login");
       }
-      showToast('error', err.response.data.message)
+      showToast("error", err.response.data.message);
     } finally {
-      loading.value = false
+      loading.value = false;
     }
-  }
+  };
 
   const registerMember = async (payload: any) => {
-    loading.value = true
+    loading.value = true;
     try {
-      const { data } = await authService.createMember(payload)
-      showToast('success', data.message || 'Registration successful')
-      return true
+      const { data } = await authService.createMember(payload);
+      showToast("success", data.message || "Registration successful");
+      return true;
     } catch (err: any) {
-      showToast('error', err.response?.data?.message || 'Registration failed')
-      error.value = err.message || 'Failed to register'
-      return false
+      showToast("error", err.response?.data?.message || "Registration failed");
+      error.value = err.message || "Failed to register";
+      return false;
     } finally {
-      loading.value = false
+      loading.value = false;
     }
-  }
+  };
 
   const requestForget = async (payload: any) => {
-    loading.value = true
+    loading.value = true;
     try {
-      const { data } = await authService.requestForget(payload)
+      const { data } = await authService.requestForget(payload);
       data.value = data;
-      return true
+      return true;
     } catch (err: any) {
-      showToast('error', err.response?.data?.message || 'Registration failed')
-      error.value = err.message || 'Failed to register'
-      return false
+      showToast("error", err.response?.data?.message || "Request failed");
+      error.value = err.message || "Failed to register";
+      return false;
     } finally {
-      loading.value = false
+      loading.value = false;
     }
-  }
+  };
 
   const changePassword = async (code: string, payload: any) => {
-    loading.value = true
+    loading.value = true;
     try {
-      const { data } = await authService.changePassword(code, payload)
-      data.value = data;
-      return true
+      const response = await authService.changePassword(code, payload);
+      data.value = response;
+      return true;
     } catch (err: any) {
-      showToast('error', err.response?.data?.message || 'Registration failed')
-      error.value = err.message || 'Failed to register'
-      return false
+      showToast(
+        "error",
+        err.response?.data?.message || "Reset password failed"
+      );
+      error.value = err.message || "Failed to reset password";
+      return false;
     } finally {
-      loading.value = false
+      loading.value = false;
     }
-  }
+  };
 
   const loginMember = async (payload: any) => {
-    loading.value = true
+    loading.value = true;
     try {
-      const response = await authService.loginMember(payload)
-      data.value = response
-      initialize()
-      setToken(data.value.data.tokenData.token)
-      showToast('success', data.value.message)
+      const response = await authService.loginMember(payload);
+      data.value = response;
+      initialize();
+      setToken(data.value.data.tokenData.token);
+      showToast("success", data.value.message);
       isLogin.value = true;
-      Cookies.set('role', 'member')
+      Cookies.set("role", "member");
     } catch (err: any) {
-      showToast('error', err.response.data.message)
-      error.value = err.message || 'Failed to create role'
+      showToast("error", err.response.data.message);
+      error.value = err.message || "Failed to create role";
       isLogin.value = false;
-      loginRole.value = 'member';
+      loginRole.value = "member";
     } finally {
-      loading.value = false
+      loading.value = false;
     }
-  }
+  };
 
   const loginAdmin = async (payload: any) => {
-    loading.value = true
+    loading.value = true;
     try {
-      const response = await authService.loginMember(payload)
-      data.value = response
-      if (data.value.data.users.role_id == 2){
-        initialize()
-        setToken(data.value.data.tokenData.token)
-        showToast('success', data.value.message)
+      const response = await authService.loginMember(payload);
+      data.value = response;
+      if (data.value.data.users.role_id == 2) {
+        initialize();
+        setToken(data.value.data.tokenData.token);
+        showToast("success", data.value.message);
         isLogin.value = true;
-        loginRole.value = 'admin';
-        Cookies.set('role', 'admin')
+        loginRole.value = "admin";
+        Cookies.set("role", "admin");
       } else {
-        showToast('error', 'Akun anda bukan administrator')
-        return false
+        showToast("error", "Akun anda bukan administrator");
+        return false;
       }
     } catch (err: any) {
-      showToast('error', err.response.data.message)
-      error.value = err.message || 'Failed to create role'
+      showToast("error", err.response.data.message);
+      error.value = err.message || "Failed to create role";
       isLogin.value = false;
     } finally {
-      loading.value = false
+      loading.value = false;
     }
-  }
+  };
 
   const logoutMember = async () => {
-    clearToken()
-  }
+    clearToken();
+  };
 
   const agreements = async () => {
-    loading.value = true
+    loading.value = true;
     try {
-      const response = await authService.getAgreements()
-      listAgreements.value = await response.data
-      Cookies.set('user', JSON.stringify(userLogin.value));
+      const response = await authService.getAgreements();
+      listAgreements.value = await response.data;
+      Cookies.set("user", JSON.stringify(userLogin.value));
     } catch (err: any) {
-      showToast('error', err.response.data.message)
+      showToast("error", err.response.data.message);
     } finally {
-      loading.value = false
+      loading.value = false;
     }
-  }
-
+  };
 
   return {
     members,
@@ -174,6 +176,6 @@ export const useAuthStore = defineStore('auth', () => {
     changePassword,
     token,
     setToken,
-    clearToken
-  }
-})
+    clearToken,
+  };
+});
